0dfeb566f234b68f9cecf0e94e14e415f171a41e
RAUNIT RAJ <74114251+HACKED101@users.noreply.github.com>
2025-04-04 12:56:22 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,1 +1,16 @@
-
+const MovieCard = ({ movie }) => {
+    return (
+      <div className="movie-card">
+        <img 
+          src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/300x450?text=No+Poster'} 
+          alt={movie.Title} 
+          className="movie-poster"
+        />
+          <h3 className="movie-title">{movie.Title}</h3>
+          
+            <p>{movie.Year}</p>
+      </div>
+    );
+  };
+  
+  export default MovieCard;
